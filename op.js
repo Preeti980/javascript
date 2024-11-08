322b7fa8bc8389d3d49a2a7f360b345841a16753
Priti Chauhan <114865373+Preeti980@users.noreply.github.com>
2024-11-08 20:17:05 +0530

    Create op.js

---
+++ op.js
@@ -0,0 +1,15 @@
+//Artimetic operation
+let a=5;
+let b=2;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);//5^2
+//unary operator
+a=a+1; // or a++ both are same which is 6 which is post increment
+console.log(a); //6
+a = a-1; // or a-- both are same which is 4 post decrement
+++a;//6
+console.log(a);

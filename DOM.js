0b48c124a67673694bef0b14cf37d1d01abb9cab
Priti Chauhan <114865373+Preeti980@users.noreply.github.com>
2024-11-15 17:36:39 +0530

    Create DOM.js

---
+++ DOM.js
@@ -0,0 +1,9 @@
+//DOM (Document object model)
+//Window Object
+// The window object represents an open window in a browser . it is browser's object(not javascript's) & is automatically created by browser.
+//it is a global object with lots of properties & method
+console.log(window);
+console.log("hello1");
+window.console.log("hello2");
+//output :hello1
+//hello2

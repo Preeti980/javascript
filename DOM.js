d4550aea5dbbb33740db52c8ed2e1fdd38f7204d
Priti Chauhan <114865373+Preeti980@users.noreply.github.com>
2024-11-16 10:13:45 +0530

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -7,3 +7,35 @@ console.log("hello1");
 window.console.log("hello2");
 //output :hello1
 //hello2
+
+
+let heading = document.getElementById("hheading-class");
+heading.innerText= heading.innerText+" from apna college "
+
+let div = document.getElementsByClassName("box");
+for(let i=0; i<div.length; i++){
+    div[0].style.backgroundColor = "red";
+    div[0].innerText="priya"
+    div[1].style.backgroundColor = "green";
+    div[1].innerText="Chauhan";
+    div[2].style.backgroundColor = "yellow";
+}
+
+let div = document.getElementById("box1");
+console.log(div);
+let box = div.getAttribute("id");
+console.log(box);
+
+let newbtn = document.createElement("button");
+newbtn.innerText="click me";
+console.log(newbtn)
+
+let div = document.getElementById("box");
+div.append(newbtn)
+
+let newbtn = document.createElement("button");
+newbtn.innerText="click me!";
+document.querySelector("body").prepend(newbtn);
+newbtn.style.backgroundColor="green";
+newbtn.style.color="white"
+

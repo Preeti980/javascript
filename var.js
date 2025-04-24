1b9c0406cf6ec7774eff81239625b37a7aca3972
Priti Chauhan <114865373+Preeti980@users.noreply.github.com>
2025-04-24 23:53:48 +0530

    Create var.js

---
+++ var.js
@@ -0,0 +1,67 @@
+
+// name = "priti";
+// age = 24;
+// x =null;//eska matalb hai khali 
+// y=undefined;//eska matalab hai pta nhi kya hai
+// console.log(name);
+// console.log(x);
+// console.log(y);
+// isFollow= false;
+// console.log(isFollow);
+
+// fullname="priti chauhan";
+// FULLNAME= "priti singh";
+// console.log(fullname);
+// console.log(FULLNAME);
+// Console = "priya";
+// console.log(Console);
+
+//variables var let const 
+// let fullName = "priti chauhan";
+// console.log(fullName);
+// var age =24; // eska use nhi karte hai
+// var age = 59;
+// var age = 89;
+// console.log(age);
+
+
+// let a =23; // let a; it is also possible 
+// // let a =90;  can not write this line 
+// a =90; // only change this value
+
+
+// const age = 24;// const a; it show error 
+// age = 59;// it show error  // no compile time error only run time error we get
+// console.log(age);
+// const PI = 3.14;
+
+
+//it is possible with let
+// {
+//     let a =5;
+//     console.log(a);
+// }
+// {
+//     let a =10;
+//     console.log(a);
+// }
+
+// // data types --> Number,String , Boolean, Undefined,Null,Biglnt,Symbol---> these are all primitive data types
+// let age = 3;
+// let price = 390.10;
+// // number = 20,20.4,-30,// sare number me hi ayenge
+// // o/p age ---> 3 ----> typeof age --> number
+
+// let fullName = "priti";
+// //o/p= fullName ---> priti ---> typeof fullName ---> string
+// let x;
+// //o/p= x --> undefined ---> typeof x ----> undefined
+
+// let y = null;
+// //o/p y---> null----> typeof y ----> object
+// let z= BigInt("123");
+// //o/p z--->123n --->typeof z--->bigint
+// let p = Symbol("Hello!");
+// //o/p p--->Symbol(Hello!) --->typeof y ---> symbol
+
+// //non-primitive datatypes - objects, Arrays, functions

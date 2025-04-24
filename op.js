d2b2af215e66ba74293ac1704e1f6341fbeba91a
Priti Chauhan <114865373+Preeti980@users.noreply.github.com>
2025-04-25 01:32:07 +0530

    Update op.js

--- op.js
+++ op.js
@@ -1,15 +1,27 @@
 //Artimetic operation
 let a=5;
 let b=2;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
-console.log(a**b);//5^2
+console.log(a+b);// 7
+console.log(a-b);//3
+console.log(a*b);//10
+console.log(a/b);//2.5
+console.log(a%b);//1
+console.log(a**b);//5^2 = 25
 //unary operator
 a=a+1; // or a++ both are same which is 6 which is post increment
-console.log(a); //6
+console.log(a); //5
 a = a-1; // or a-- both are same which is 4 post decrement
-++a;//6
-console.log(a);
+++a;//6 preincrement
+
+let a=5;
+console.log( a++);//postincrement 5
+console.log(a);//6
+let b=5;
+console.log(++b);//preincrement 6
+console.log(b);//6
+let c=5;
+console.log(c--);//postdecrement 5
+console.log(c);//4
+let d=5;
+console.log(--d );//predecrement 4
+console.log(d);//4
